3ebc63f45aed80288febf45fbad9070f28abfa81
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2019-01-29 00:37:34 -0700

    Added duSocket file

--- Assignments/SocketsAndThreading/Node.c
+++ Assignments/SocketsAndThreading/Node.c
@@ -5,6 +5,7 @@
 //#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "duSocket.h"
 
 int main() {
 
---
+++ Assignments/SocketsAndThreading/duSocket.c
@@ -0,0 +1,31 @@
+#include "duSocket.h"
+
+// Like new ServerSocket in Java
+int setupServerSocket(int portno)
+{
+    return 0;
+}
+
+// Like new Socket in Java
+int callServer(char* host, int portno)
+{
+    return 0;
+}
+
+// Like ss.accept() in Java
+int serverSocketAccept(int serverSocket)
+{
+    return 0;
+}
+
+// Write an int over the given socket
+void writeInt(int x, int socket)
+{
+    
+}
+
+// Read an int from the given socket
+int readInt(int socket)
+{
+    return 0;
+}
