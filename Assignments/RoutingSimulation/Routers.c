8e2d45724cb251fb33a9a605babeee03f59dda65
Maxwell Evans <maxwellthefourth@gmail.com>
2019-03-17 16:51:01 -0600

    initial setup

--- Assignments/RoutingSimulation/Routers.c
+++ Assignments/RoutingSimulation/Routers.c
@@ -1,6 +1,17 @@
 
 #include "Routers.h"
 
+#define NUMROUTERS 3
+
 int main() {
+#pragma omp parallel for schedule(static, 2) reduction(+:temp)
+    for (int i = 0; i < NUMROUTERS; i++) {
+        createRouter();
+    }
     return 0;
 }
+
+void createRouter() {
+    
+}
+
--- Assignments/RoutingSimulation/Routers.h
+++ Assignments/RoutingSimulation/Routers.h
@@ -4,4 +4,6 @@
 
 #include <stdio.h>
 
+void createRouter();
+
 #endif /* Routers_h */
