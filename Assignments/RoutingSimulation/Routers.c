8b355c5850b3c041d65ef531050275747157de29
Maxwell Evans <maxwellthefourth@gmail.com>
2019-03-18 11:51:13 -0600

    bug fixes

--- Assignments/RoutingSimulation/Routers.c
+++ Assignments/RoutingSimulation/Routers.c
@@ -10,7 +10,7 @@ int main() {
 //        char filename[16];
 //        sprintf(filename, "neighbors-%d.txt", omp_get_thread_num());
         char filename[78];
-        sprintf(filename, "Desktop/ComputerNetworking/Assignments/SlidingWindowProtocols/neighbors-%d.txt", omp_get_thread_num());
+        sprintf(filename, "Desktop/ComputerNetworking/Assignments/RoutingSimulation/neighbors-%d.txt", omp_get_thread_num());
         createRouter(filename);
     }
     return 0;
@@ -18,12 +18,22 @@ int main() {
 
 void createRouter(char* router_name) {
     printf("Creating thread for: %s\n", router_name);
-    FILE *fIn = fopen(router_name, "wb");
+    FILE *fIn = fopen(router_name, "rb");
     if (fIn == NULL) {
         printf("Error readig in file %s.\n", router_name);
         exit(1);
     }
     
+    int neighbors[NUMROUTERS];
+    fseek(fIn, 0L, SEEK_END);
+    long fSize = ftell(fIn);
+    if (fSize != NUMROUTERS) {
+        printf("Error with incorrect router configuration.\n");
+        exit(1);
+    }
+    rewind(fIn);
     
+    size_t read_data = fread(neighbors, 1, fSize, fIn);
+    printf("Router neighbors: %d %d %d\n", neighbors[0], neighbors[1], neighbors[2]);
 }
 
