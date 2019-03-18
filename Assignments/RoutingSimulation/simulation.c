d4101ba748f7a85f6ee2cf5a883bc6fce93648ad
Maxwell Evans <maxwellthefourth@gmail.com>
2019-03-18 16:43:40 -0600

    transfered stuff to simulation

--- Assignments/RoutingSimulation/simulation.c
+++ Assignments/RoutingSimulation/simulation.c
@@ -3,28 +3,51 @@
 #define PORT 7000
 #define MAXLINE 10
 int NUMROUTERS;
+char* ROOTNAME;
 
 int main(int argc, char** argv) {
     if(argc>1)
     {
         NUMROUTERS = atoi(argv[1]);
+        ROOTNAME = argv[2];
     }
 
     omp_set_num_threads(NUMROUTERS);
 #pragma omp parallel
     {
-        char routername[78];
+        char routername[16];
         sprintf(routername, "Router_%d", omp_get_thread_num());
-        createRouter(routername);
+        createRouter(routername, omp_get_thread_num());
     }
     return 0;
 }
 
-void createRouter(char* router_name) {
+void createRouter(char* router_name, int router_num) {
     int sockfd; 
     unsigned char buffer[MAXLINE]; 
     struct sockaddr_in servaddr;
-
+    
+    // read file
+    char router_file[100];
+//    sprintf(router_file, "Desktop/ComputerNetworking/Assignments/RoutingSimulation/%s-%d.txt", ROOTNAME, omp_get_thread_num());
+    sprintf(router_file, "%s-%d.txt", ROOTNAME, omp_get_thread_num());
+    FILE *fIn = fopen(router_file, "rb");
+    if (fIn == NULL) {
+        printf("ERROR: Unable to open file [%s].\n", router_file);
+        exit(1);
+    }
+    
+    fseek(fIn, 0L, SEEK_END);
+    long fSize = ftell(fIn);
+    rewind(fIn);
+    if (fSize != NUMROUTERS) {
+        printf("ERROR: Incorrect file format for router configuration.\n");
+        exit(1);
+    }
+    int neighbors[fSize];
+    size_t read_data = fread(neighbors, 1, fSize, fIn);
+    printf("Created router %d with %d neighbors.\n", router_num, fSize);
+    
     // setup sockets
     
     // Creating socket file descriptor
@@ -38,7 +61,7 @@ void createRouter(char* router_name) {
     
     // Filling server information 
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT+omp_get_thread_num()); 
+    servaddr.sin_port = htons(PORT+router_num);
 
     // Setup the server host address
     unsigned char* host = "localhost";
